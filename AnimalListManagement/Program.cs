8eb6a57b3e1eb23b61c4ffee25f16fb21c07f1a5
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-10-13 09:29:16 +0700

    Add project files.

---
+++ AnimalListManagement/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+
+namespace AnimalListManagement
+{
+    using MainData;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of dogs: ");
+            int dogCount = int.Parse(Console.ReadLine());
+
+            Dog[] dogs = new Dog[dogCount];
+            for (int i = 0; i < dogCount; i++)
+            {
+                dogs[i] = new Dog();
+                Console.WriteLine($"Entering information for Dog {i + 1}:");
+                dogs[i].InputInfo();
+            }
+
+            Console.Write("Enter the number of cats: ");
+            int catCount = int.Parse(Console.ReadLine());
+
+            Cat[] cats = new Cat[catCount];
+            for (int i = 0; i < catCount; i++)
+            {
+                cats[i] = new Cat();
+                Console.WriteLine($"Entering information for Cat {i + 1}:");
+                cats[i].InputInfo();
+            }
+
+            Console.WriteLine("\nDisplaying information of all dogs:");
+            foreach (var dog in dogs)
+            {
+                dog.DisplayInfo();
+            }
+
+            Console.WriteLine("\nDisplaying information of all cats:");
+            foreach (var cat in cats)
+            {
+                cat.DisplayInfo();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace MainData
+{
+    public class Animal
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; } = 0;
+        public float Height { get; set; } = 0;
+        public float Weight { get; set; } = 0;
+
+        public void InputAge()
+        {
+            bool isComplete = false;
+
+            Console.Write($"Enter the age of the {this.GetType().Name}: ");
+            try
+            {
+                Age = int.Parse(Console.ReadLine());
+                if (Age < 0 || Age > 20)
+                    throw new NegativeNumException();
+                isComplete = true;
+            }
+            catch (FormatException)
+            {
+                Console.Write("Somethings went wrong! Please enter again: ");
+            }
+            catch (NegativeNumException)
+            {
+                Console.Write("Invalid number! Please enter again: ");
+            }
+            finally
+            {
+                if (!isComplete)
+                    InputAge();
+            }
+        }
+
+        public enum InputType
+        {
+            Height,
+            Weight
+        }
+
+        public void InputNum(InputType inputType)
+        {
+            bool isComplete = false;
+
+            string prompt = inputType == InputType.Height ? "height" : "weight";
+            Console.Write($"Enter the {prompt} of the {this.GetType().Name}: ");
+
+            while (!isComplete)
+            {
+                try
+                {
+                    if (inputType == InputType.Height)
+                        Height = float.Parse(Console.ReadLine());
+                    else if (inputType == InputType.Weight)
+                        Weight = float.Parse(Console.ReadLine());
+                    isComplete = true;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Something went wrong! Please enter again: ");
+                }
+                //catch (NegativeNumException)
+                //{
+                //    Console.Write("Invalid number! Please enter again: ");
+                //}
+            }
+        }
+
+        public virtual void InputInfo()
+        {
+            Console.Write($"Enter the name of the {this.GetType().Name}: ");
+            Name = Console.ReadLine();
+
+            InputAge();
+
+            InputNum(InputType.Height);
+
+            InputNum(InputType.Weight);
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Type: {this.GetType().Name}, Name: {Name}, Age: {Age}, Height: {Height}, Weight: {Weight}");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Height: {Height}, Weight: {Weight}";
+        }
+    }
+
+    public class NegativeNumException : Exception
+    {
+        public NegativeNumException() { }
+        public NegativeNumException(string message): base(message) { }
+    }
+
+    public class Dog : Animal
+    {
+
+    }
+
+    public class Cat : Animal
+    {
+
+    }
+}
---
+++ SportsManagement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SportsManagement
+{
+    using MainData;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Football f = new Football();
+            Tenis t = new Tenis();
+            Volleyball v = new Volleyball();
+
+            f.InputInfo(); t.InputInfo(); v.InputInfo();
+            f.DisplayInfo(); t.DisplayInfo(); v.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace MainData
+{
+    public class Sport
+    {
+        public int Num { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Ball { get; set; }
+
+        public virtual void InputInfo()
+        {
+            Console.WriteLine($"Enter Info of {this.GetType().Name}:");
+
+            Console.Write("Number of players: "); Num = int.Parse(Console.ReadLine());
+            Console.Write("Time limit: "); Time = TimeSpan.Parse(Console.ReadLine());
+            Console.Write("Ball's type: "); Ball = Console.ReadLine();
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}\n");
+
+            Console.WriteLine($"Players: {Num}");
+            Console.WriteLine($"Time limit: {Time}");
+            Console.WriteLine($"Ball's type: {Ball}");
+        }
+    }
+
+    public class Football : Sport { }
+    public class Tenis : Sport { }
+    public class Volleyball : Sport { }
+}
